24c1a32551ffbb782dffa1be2535d438cf160da4
exfarnanda1945 <exfarnanda1945@gmail.com>
2023-12-08 07:57:17 +0700

    add: landing page screen + clip

---
+++ lib/screens/landing_page/landing-page-screen.dart
@@ -0,0 +1,125 @@
+import 'package:employee_system_prototype/utils/colors.dart';
+import 'package:employee_system_prototype/utils/clip/landing-page-top-clip.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class LandingPageScreen extends StatelessWidget {
+  const LandingPageScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: blueberry,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          ClipPath(
+            clipper: LandingPageTopClip(),
+            child: Container(
+              color: hanBlue,
+              height: 620.0,
+              width: double.infinity,
+              child: const Padding(
+                padding: EdgeInsets.only(left: 40),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(bottom: 10),
+                      child: Text(
+                        'employee system',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontFamily: 'Inter',
+                          fontWeight: FontWeight.w600,
+                          height: 0,
+                        ),
+                      ),
+                    ),
+                    Text.rich(
+                      TextSpan(
+                        children: [
+                          TextSpan(
+                            text: 'XYC',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 36,
+                              fontFamily: 'Inter',
+                              fontWeight: FontWeight.w800,
+                              height: 0,
+                            ),
+                          ),
+                          TextSpan(
+                            text: ' Payment',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 36,
+                              fontFamily: 'Inter',
+                              fontWeight: FontWeight.w400,
+                              height: 0,
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 40,right: 40,bottom: 95),
+            child: Column(
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    context.pushNamed("sign");
+                  },
+                  style: ElevatedButton.styleFrom(
+                      minimumSize: const Size.fromHeight(50),
+                      shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(5)))),
+                  child: const Text(
+                    'Login',
+                    style: TextStyle(
+                      color: blueberry,
+                      fontSize: 16,
+                      fontFamily: 'Inter',
+                      fontWeight: FontWeight.w600,
+                      height: 0,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: const Size.fromHeight(50),
+                    backgroundColor: blueberry,
+                    shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(5)),
+                        side: BorderSide(width: 2, color: Colors.white)),
+                  ),
+                  child: const Text(
+                    'Sign Up',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontFamily: 'Inter',
+                      fontWeight: FontWeight.w600,
+                      height: 0,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
