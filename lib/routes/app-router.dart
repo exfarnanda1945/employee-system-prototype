bd2de26fa4ff94c77ac39e322ffb6083f06b6483
exfarnanda1945 <exfarnanda1945@gmail.com>
2023-12-08 07:57:50 +0700

    add: app router

---
+++ lib/routes/app-router.dart
@@ -0,0 +1,38 @@
+import 'package:employee_system_prototype/screens/auth/login-screen.dart';
+import 'package:employee_system_prototype/screens/landing_page/landing-page-screen.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+final _navigatorKey = GlobalKey<NavigatorState>();
+
+class AppRouter {
+  static GoRouter getRoutes(String initialRoute) => GoRouter(
+          initialLocation: initialRoute,
+          debugLogDiagnostics: true,
+          routes: [
+            GoRoute(
+              path: "/landing-page",
+              name: "landing-page",
+              builder: (context, state){
+                return const LandingPageScreen();
+              },
+            ),
+            GoRoute(
+              path: "/sign",
+              name: "sign",
+              builder: (context, state) {
+                return const LoginScreen();
+              },
+            ),
+            // GoRoute(path: "/main", name: "main", routes: [
+            //   GoRoute(path: "/home", name: "home", routes: [
+            //     GoRoute(
+            //       path: "/attendance",
+            //       name: "attendance",
+            //     )
+            //   ]),
+            // ]),
+            // GoRoute(path: "/request", name: "request"),
+            // GoRoute(path: "/account", name: 'account')
+          ]);
+}
