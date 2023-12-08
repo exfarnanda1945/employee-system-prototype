5cf143ef36df786e4df584268d23117d686be732
miko <exfar@samir.co.id>
2023-12-08 18:01:37 +0700

    add: wip main screen

---
+++ lib/screens/main/main-screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: [
+
+        ],
+      ),
+    );
+  }
+}
