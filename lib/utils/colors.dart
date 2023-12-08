773202e974d0cff6eebce6af6812beb11026e538
miko <exfar@samir.co.id>
2023-12-08 18:01:26 +0700

    add: more colors

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -2,3 +2,6 @@ import 'dart:ui';
 
 const blueberry = Color(0xff537fe7);
 const hanBlue = Color(0xff4266b9);
+const davyGrey = Color(0xff5D5D5D);
+const platinum =Color(0xffe6e6e6);
+const lightGray = Color(0xffD4D4D4);
