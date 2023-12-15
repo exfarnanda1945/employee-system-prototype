9053b721a5001aafbefbafaa9f31d75eb7a3f230
miko <exfar@samir.co.id>
2023-12-15 09:34:21 +0700

    fix: add inter font globally

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
       title: 'Employee System',
       theme: ThemeData(
         useMaterial3: true,
+        fontFamily: 'Inter'
       ),
       routerConfig: AppRouter.getRoutes("/main"),
     );
