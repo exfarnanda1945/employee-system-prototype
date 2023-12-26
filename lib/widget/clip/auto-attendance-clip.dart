dd048f8f76caa83b4dfc53a6cc95ec163711ce25
miko <exfar@samir.co.id>
2023-12-26 15:48:05 +0700

    add: auto attendance clip wip

---
+++ lib/widget/clip/auto-attendance-clip.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class AutoAttendanceClip extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    double w = size.width;
+    double h = size.height;
+
+    final path = Path();
+    path.lineTo(0, h);
+    path.lineTo(w,h);
+    path.lineTo(w, 0);
+    path.close();
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
+    return false;
+  }
+}
