a062027f4dbd34808aa94709107a999fc7a1ef7f
miko <exfar@samir.co.id>
2023-12-27 15:39:25 +0700

    fix: auto attendance form

--- lib/widget/forms/auto-attendance-form.dart
+++ lib/widget/forms/auto-attendance-form.dart
@@ -6,9 +6,9 @@ class AutoAttendanceForm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
+    return Column(
       children: [
-        Text(
+        const Text(
           'Clock In',
           style: TextStyle(
             color: hanBlue,
@@ -18,10 +18,10 @@ class AutoAttendanceForm extends StatelessWidget {
           ),
         ),
         Padding(
-          padding: EdgeInsets.only(top: 18),
+          padding: const EdgeInsets.only(top: 12),
           child: Column(
             children: [
-              Row(
+              const Row(
                 children: [
                   Icon(
                     Icons.location_on,
@@ -29,7 +29,9 @@ class AutoAttendanceForm extends StatelessWidget {
                     size: 20,
                   ),
                   Padding(
-                    padding: EdgeInsets.only(top: 8.0),
+                    padding: EdgeInsets.only(
+                      left: 8.0,
+                    ),
                     child: Text(
                       'Your Location',
                       style: TextStyle(
@@ -41,7 +43,78 @@ class AutoAttendanceForm extends StatelessWidget {
                     ),
                   )
                 ],
-              )
+              ),
+              const Padding(
+                padding: EdgeInsets.only(top: 5, left: 20, right: 20),
+                child: Text(
+                  'Jl. Citra Indah Utama No.18 RT.04/RW.019 , Desa Sukamaju, Kecamatan  Jonggol, Kabupaten Bogor, Jawa Barat 16830',
+                  style: TextStyle(
+                    color: davyGrey,
+                    fontSize: 14,
+                    fontWeight: FontWeight.w400,
+                    height: 0,
+                  ),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(top: 10),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.notes,
+                      color: Colors.black,
+                      size: 20,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(left: 8),
+                      child: Text(
+                        'Note (Optional)',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 14,
+                          fontWeight: FontWeight.w600,
+                          height: 0,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 10),
+                child: TextFormField(
+                  maxLines: 4,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderSide: const BorderSide(
+                        color: blueberry,
+                        width: 1,
+                      ),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 15),
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                      minimumSize: const Size.fromHeight(45),
+                      backgroundColor: blueberry,
+                      shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(5)))),
+                  child: const Text(
+                    'Clock In',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                      height: 0,
+                    ),
+                  ),
+                ),
+              ),
             ],
           ),
         )
