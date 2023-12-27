d2555c9ed21191d455fdd09e7f1332e97902d38c
miko <exfar@samir.co.id>
2023-12-27 15:39:58 +0700

    fix: add gmaps widget

--- lib/screens/auto-attendance/auto-attendance-screen.dart
+++ lib/screens/auto-attendance/auto-attendance-screen.dart
@@ -7,6 +7,7 @@ import 'package:employee_system_prototype/widget/forms/auto-attendance-form.dart
 import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:network_info_plus/network_info_plus.dart';
 import 'package:permission_handler/permission_handler.dart';
 
@@ -64,7 +65,17 @@ class _AutoAttendanceScreenState extends State<AutoAttendanceScreen> {
           alignment: Alignment.topCenter,
           child: SizedBox(
             width: double.infinity,
-            height: MediaQuery.of(context).size.height * (2 / 3),
+            height: MediaQuery.of(context).size.height * (5/7),
+            child: GoogleMap(
+              mapType: MapType.normal,
+              initialCameraPosition: const CameraPosition(
+                  target: LatLng(-6.267309535408621, 106.82552887691514), zoom: 17),
+              onMapCreated: (GoogleMapController controller) {},
+              myLocationButtonEnabled: true,
+              myLocationEnabled: true,
+              zoomGesturesEnabled: false,
+              scrollGesturesEnabled: false,
+            )
           ),
         ),
         Align(
@@ -72,7 +83,7 @@ class _AutoAttendanceScreenState extends State<AutoAttendanceScreen> {
           child: ClipPath(
             clipper: AutoAttendanceClip(),
             child: Container(
-              height: MediaQuery.of(context).size.height * (1 / 3),
+              height: MediaQuery.of(context).size.height * (2/7),
               width: double.infinity,
               color: hanBlue,
             ),
@@ -84,7 +95,7 @@ class _AutoAttendanceScreenState extends State<AutoAttendanceScreen> {
             padding: const EdgeInsets.symmetric(horizontal: 30),
             child: Container(
               width: double.infinity,
-              height: MediaQuery.of(context).size.height * (1 / 2),
+              height: MediaQuery.of(context).size.height * (3/7),
               decoration: const BoxDecoration(
                   color: Colors.white,
                   borderRadius: BorderRadius.only(
@@ -92,7 +103,7 @@ class _AutoAttendanceScreenState extends State<AutoAttendanceScreen> {
                     topLeft: Radius.circular(30),
                   )),
               child: const Padding(
-                padding: EdgeInsets.all(25),
+                padding: EdgeInsets.only(left: 30,right: 30,top: 15),
                 child: AutoAttendanceForm(),
               ),
             ),
