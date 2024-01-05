6e0026d3d2202929ad393986cd722c1f14305c92
exfarnanda1945 <exfarnanda1945@gmail.com>
2024-01-05 07:17:23 +0700

    fix: remove bg white

--- lib/screens/main/main-screen.dart
+++ lib/screens/main/main-screen.dart
@@ -19,7 +19,6 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
       bottomNavigationBar: BottomNavigation(
         currentPage: currentPageNavigation,
         onDestinationChange: (value) {
