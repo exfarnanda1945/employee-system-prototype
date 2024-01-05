67e90eaf5ceafae9fee512434150d68798129bed
exfarnanda1945 <exfarnanda1945@gmail.com>
2024-01-05 07:17:07 +0700

    add: approval screen

--- lib/screens/main/approval/approval-screen.dart
+++ lib/screens/main/approval/approval-screen.dart
@@ -1,3 +1,5 @@
+import 'package:employee_system_prototype/model/approval-menu.dart';
+import 'package:employee_system_prototype/utils/colors.dart';
 import 'package:flutter/material.dart';
 
 class ApprovalScreen extends StatelessWidget {
@@ -5,10 +7,36 @@ class ApprovalScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Approval"),
+    return Scaffold(
+        body: Padding(
+      padding: const EdgeInsets.all(12),
+      child: GridView.count(
+        crossAxisCount: 3,
+        children: approvalMenuList
+            .map((menu) => Column(
+                  children: [
+                    Icon(
+                      menu.icon,
+                      size: 54,
+                      color: menu.color,
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    Text(
+                      menu.title,
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(
+                        color: davyGrey,
+                        fontSize: 14,
+                        fontWeight: FontWeight.w500,
+                        height: 0,
+                      ),
+                    )
+                  ],
+                ))
+            .toList(),
       ),
-    );
+    ));
   }
 }
