73c93864334ddf22c2b9ddfbb1d7644ed82ad27e
exfarnanda1945 <exfarnanda1945@gmail.com>
2024-01-11 22:28:38 +0700

    add: sick screen

---
+++ lib/screens/sick/sick-screen.dart
@@ -0,0 +1,103 @@
+import 'package:employee_system_prototype/utils/colors.dart';
+import 'package:employee_system_prototype/widget/cards/approval-counter-card.dart';
+import 'package:employee_system_prototype/widget/cards/approval-item-card.dart';
+import 'package:flutter/material.dart';
+import 'package:material_symbols_icons/symbols.dart';
+
+class SickScreen extends StatelessWidget {
+  const SickScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text(
+          "Sick",
+          style: TextStyle(fontWeight: FontWeight.w700, fontSize: 18),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            const ApprovalCounterCard(),
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    "All Records",
+                    style: TextStyle(fontWeight: FontWeight.w600, fontSize: 16),
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        width: 30,
+                        height: 30,
+                        decoration: BoxDecoration(
+                            borderRadius:
+                                const BorderRadius.all(Radius.circular(10)),
+                            border: Border.all(color: blueberry, width: 2)),
+                        child: const Icon(
+                          Icons.add,
+                          color: blueberry,
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 6,
+                      ),
+                      Container(
+                        height: 30,
+                        decoration: BoxDecoration(
+                            borderRadius:
+                                const BorderRadius.all(Radius.circular(10)),
+                            border: Border.all(color: davyGrey, width: 2)),
+                        child: const Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 6),
+                          child: Row(
+                            children: [
+                              Icon(Symbols.page_info),
+                              SizedBox(
+                                width: 5,
+                              ),
+                              Text(
+                                "Filter",
+                                style: TextStyle(fontWeight: FontWeight.w500),
+                              )
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            const Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    ApprovalItemCard(),
+                    ApprovalItemCard(),
+                    ApprovalItemCard(),
+                    ApprovalItemCard(),
+                    ApprovalItemCard(),
+                    ApprovalItemCard(),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
