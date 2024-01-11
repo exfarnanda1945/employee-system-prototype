da2ab6f9eff5450a5c17560af71cbc8b599800ef
exfarnanda1945 <exfarnanda1945@gmail.com>
2024-01-11 22:29:09 +0700

    fix: ontap approval menu list

--- lib/screens/main/approval/approval-screen.dart
+++ lib/screens/main/approval/approval-screen.dart
@@ -1,6 +1,7 @@
 import 'package:employee_system_prototype/model/approval-menu.dart';
 import 'package:employee_system_prototype/utils/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class ApprovalScreen extends StatelessWidget {
   const ApprovalScreen({super.key});
@@ -13,27 +14,32 @@ class ApprovalScreen extends StatelessWidget {
       child: GridView.count(
         crossAxisCount: 3,
         children: approvalMenuList
-            .map((menu) => Column(
-                  children: [
-                    Icon(
-                      menu.icon,
-                      size: 54,
-                      color: menu.color,
-                    ),
-                    const SizedBox(
-                      height: 8,
-                    ),
-                    Text(
-                      menu.title,
-                      textAlign: TextAlign.center,
-                      style: const TextStyle(
-                        color: davyGrey,
-                        fontSize: 14,
-                        fontWeight: FontWeight.w500,
-                        height: 0,
+            .map((menu) => GestureDetector(
+                  onTap: () {
+                    context.pushNamed(menu.navigateTo);
+                  },
+                  child: Column(
+                    children: [
+                      Icon(
+                        menu.icon,
+                        size: 54,
+                        color: menu.color,
                       ),
-                    )
-                  ],
+                      const SizedBox(
+                        height: 8,
+                      ),
+                      Text(
+                        menu.title,
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                          color: davyGrey,
+                          fontSize: 14,
+                          fontWeight: FontWeight.w500,
+                          height: 0,
+                        ),
+                      )
+                    ],
+                  ),
                 ))
             .toList(),
       ),
